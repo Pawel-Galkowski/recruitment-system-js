cac58bcde9f46d30d0ab85955619688950fda02d
Pawel-Galkowski <pawel.g-97@o2.pl>
2022-02-23 22:38:48 +0100

    update schema

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'no-param-reassign': 'off',
+    'no-console': 'off',
+    'no-underscore-dangle': 'off',
+    'no-await-in-loop': 'off',
+  },
+};
